5f3d1348a84d110de4d3c8827c671dd8146dba6e
Ahmed Mestiri <96390728+Ahmestiri@users.noreply.github.com>
2022-06-18 19:58:54 +0100

    Added Message Model

--- app/models.py
+++ app/models.py
@@ -1,15 +1,23 @@
 from django.db import models
 
-"""
-Room Model
-"""
 
 class Room(models.Model):
-    #Database Columns
+    # Database Columns
     name = models.CharField(max_length=255)
     description = models.TextField(null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    #Print Room
+    # Print Room
     def __str__(self):
         return str(self.name)
+
+
+class Message(models.Model):
+    # Database Columns
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    # Print Room
+    def __str__(self):
+        return str(self.body[0:50])

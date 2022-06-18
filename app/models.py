e6fae3e1740bab731595928a5ce59e8e669e14f8
Ahmed Mestiri <96390728+Ahmestiri@users.noreply.github.com>
2022-06-18 19:49:48 +0100

    Added Home & Room

--- app/models.py
+++ app/models.py
@@ -1,1 +1,15 @@
 from django.db import models
+
+"""
+Room Model
+"""
+
+class Room(models.Model):
+    #Database Columns
+    name = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    #Print Room
+    def __str__(self):
+        return str(self.name)
--- app/views.py
+++ app/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from .models import Room
+
 
 def home(request):
-    return render(request,'home.html')
+    rooms = Room.objects.all()
+    context = {"rooms": rooms}
+    return render(request, "app/home.html", context)
+
+
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+    context = {"room": room}
+    return render(request, "app/room.html", context)
